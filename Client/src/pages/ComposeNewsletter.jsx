14477ff94a3acc4f57c6bbad002a000280623af6
Anshika <anshika2011087@akgec.ac.in>
2023-10-04 22:47:18 +0530

    frontend form setup

--- Client/src/pages/ComposeNewsletter.jsx
+++ Client/src/pages/ComposeNewsletter.jsx
@@ -1,11 +1,61 @@
+import { useState } from 'react';
+import axios from 'axios';
 
-const ComposeNewsletter = () => {
-    return (
-        <div className="ComposeNewsletter">
-            ComposeNewsletter
+function ComposeNewsletter() {
+  const [subject, setSubject] = useState('');
+  const [newsletterContent, setNewsletterContent] = useState('');
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('/send-newsletter', {
+        subject,
+        newsletterContent,
+      });
+
+      if (response.status === 200) {
+        // Handle a successful newsletter submission (e.g., show a success message)
+      } else {
+        setError('Newsletter sending failed. Please try again later.');
+      }
+    } catch (err) {
+      setError('Newsletter sending failed. Please try again later.');
+    }
+  };
+
+  return (
+    <div>
+      <h1>Compose Newsletter</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="subject">Subject:</label>
+        <input
+          type="text"
+          id="subject"
+          name="subject"
+          required
+          value={subject}
+          onChange={(e) => setSubject(e.target.value)}
+        />
+
+        <label htmlFor="newsletterContent">Newsletter Content (HTML):</label>
+        <textarea
+          id="newsletterContent"
+          name="newsletterContent"
+          required
+          value={newsletterContent}
+          onChange={(e) => setNewsletterContent(e.target.value)}
+        ></textarea>
+
+        <div id="email-error" style={{ color: 'red' }}>
+          {error}
         </div>
-    );
+
+        <input type="submit" value="Send Newsletter" />
+      </form>
+    </div>
+  );
 }
 
 export default ComposeNewsletter;
-
--- Client/src/pages/Newsletter.jsx
+++ Client/src/pages/Newsletter.jsx
@@ -1,6 +1,6 @@
 import './Newsletter.css'; // Import your CSS file
-import axios from 'axios'; // Import Axios or your preferred HTTP client library
-import { useState } from 'react'; // Import useState from React
+import { useState } from 'react';
+import axios from 'axios';
 
 function Newsletter() {
   const [email, setEmail] = useState('');
@@ -8,17 +8,17 @@ function Newsletter() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    
+
     if (!isValidEmail(email)) {
-        setError('Please enter a valid email address.');
-        return; // Prevent further processing if email is not valid
-      }
+      setError('Please enter a valid email address.');
+      return;
+    }
+
     try {
-      // Send a POST request to your backend to handle the subscription
       const response = await axios.post('/subscribe', { email });
 
       if (response.status === 200) {
-        console.log(response.data.message);
+        // Handle a successful subscription (e.g., show a success message)
       } else {
         setError('Subscription failed. Please try again later.');
       }
@@ -37,32 +37,22 @@ function Newsletter() {
   };
 
   return (
-    <div className="Newsletter">
-      <form action="/subscribe" method="POST" onSubmit={handleSubmit}>
-        <section className="wrapper">
-          <div className="container">
-            <div className="img__container">
-              <img src="src\assets\Img.png" alt="salad" className="img" />
-            </div>
-            <div className="content">
-              <h2 className="subtitle">Get a weekly dose of motivation from KEECHU</h2>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                required
-                className="email"
-                placeholder="Email"
-                value={email}
-                onChange={handleEmailChange} // Use the onChange event to update the email state
-              />
-              <div id="email-error" style={{ color: 'red' }}>
-                {error}
-              </div>
-              <input type="submit" value="Subscribe" className="subscribe" />
-            </div>
-          </div>
-        </section>
+    <div className="NewsletterSubscription">
+      <form onSubmit={handleSubmit}>
+        <h2>Subscribe to Our Newsletter</h2>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Your Email"
+            value={email}
+            onChange={handleEmailChange}
+            required
+          />
+        </div>
+        {error && <div className="error">{error}</div>}
+        <div className="form-group">
+          <button type="submit">Subscribe</button>
+        </div>
       </form>
     </div>
   );
