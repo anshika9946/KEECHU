be3c402e9478afc8f9d544a8ccc2183a0d964959
Anshika <anshika2011087@akgec.ac.in>
2023-10-08 16:27:12 +0530

    updated the subscription handling

--- Client/src/pages/Newsletter.jsx
+++ Client/src/pages/Newsletter.jsx
@@ -5,6 +5,7 @@ import axios from 'axios';
 function Newsletter() {
   const [email, setEmail] = useState('');
   const [error, setError] = useState(null);
+  const [isSubscribed, setIsSubscribed] = useState(false); // Track subscription status
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -13,20 +14,21 @@ function Newsletter() {
       setError('Please enter a valid email address.');
       return;
     }
-
     try {
       const response = await axios.post('http://localhost:3001/api/subscribe', { email });
-
-
+    
       if (response.status === 200) {
-        // Handle a successful subscription (e.g., show a success message)
+        // Handle a successful subscription (e.g., show a success message or redirect)
+      } else if (response.status === 400 && response.data.error === 'Email address is already subscribed.') {
+        setError('Email address is already subscribed.');
       } else {
         setError('Subscription failed. Please try again later.');
       }
     } catch (err) {
-      console.error('Error:', err); // Log the error for debugging
+      console.error('Error:', err);
       setError('Subscription failed. Please try again later.');
     }
+    
   };
 
   const handleEmailChange = (e) => {
@@ -40,22 +42,28 @@ function Newsletter() {
 
   return (
     <div className="NewsletterSubscription">
-      <form onSubmit={handleSubmit}>
-        <h2>Subscribe to Our Newsletter</h2>
-        <div className="form-group">
-          <input
-            type="email"
-            placeholder="Your Email"
-            value={email}
-            onChange={handleEmailChange}
-            required
-          />
+      {!isSubscribed ? (
+        <form onSubmit={handleSubmit}>
+          <h2>Subscribe to Our Newsletter</h2>
+          <div className="form-group">
+            <input
+              type="email"
+              placeholder="Your Email"
+              value={email}
+              onChange={handleEmailChange}
+              required
+            />
+          </div>
+          {error && <div className="error">{error}</div>}
+          <div className="form-group">
+            <button type="submit">Subscribe</button>
+          </div>
+        </form>
+      ) : (
+        <div className="success-message">
+          Kindly check your email for verification. Once verified, you are subscribed!
         </div>
-        {error && <div className="error">{error}</div>}
-        <div className="form-group">
-          <button type="submit">Subscribe</button>
-        </div>
-      </form>
+      )}
     </div>
   );
 }
