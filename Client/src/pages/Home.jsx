bbccea9dbc079126cf8202bd6eceebbc2898ecb7
Anshika <anshika2011087@akgec.ac.in>
2023-10-14 18:40:06 +0530

    home ppage updated

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,11 +1,20 @@
-
+import styled from "styled-components";
+import Img from '/src/assets/coming.svg';
 const Home = () => {
-    return (
-        <div className="Home">
-            Home
-        </div>
-    );
-}
+  return (
+    <Wrapper>
+      <img src={Img} alt="error" />
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.section`
+max-widhth:10rem;
+  padding: 0rem 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
 
 export default Home;
-
